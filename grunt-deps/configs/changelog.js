c466092e9c0ff5c0037c771988c8803ef443b012
Roy Riojas <roy@kno.com>
2014-09-08 17:30:53 -0700

    BLD: added changelog config
    # This is H1
    ## This is H2
    
    **This text is bold**
    
    ```javascript
    'changelog': {
        dest: './report/changelog/changelog.html',
        options: {
            'gitUrlForCommit':
    'http://git.kno.com/?p=cloud/kno-web-lib.git;a=commit;h={0}',
            'gitAuthorUrl':
    'http://git.kno.com/?p=cloud/kno-web-lib.git;a=search;s={0};st=author',
            'urlForBugId':
    'https://rally1.rallydev.com/#/search?keywords={0}'
        }
    }
    
    ```

--- grunt-deps/configs/changelog.js
+++ grunt-deps/configs/changelog.js
@@ -4,7 +4,7 @@ module.exports = function (grunt, pkg, opts) {
 
   return {
     'changelog': {
-      dest: './report/changelog/changelog.html'
+      dest: './report/changelog/changelog.html',
       options: {
         'gitUrlForCommit': 'http://git.kno.com/?p=cloud/kno-web-lib.git;a=commit;h={0}',
         'gitAuthorUrl': 'http://git.kno.com/?p=cloud/kno-web-lib.git;a=search;s={0};st=author',
