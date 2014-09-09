67d8a0831b8e5dd5a7f716f731fdfcd551b774a2
Roy Riojas <roy@kno.com>
2014-09-08 17:25:15 -0700

    BLD: Adding changelog [DE12121]

---
+++ grunt-deps/configs/changelog.js
@@ -0,0 +1,15 @@
+module.exports = function (grunt, pkg, opts) {
+  'use strict';
+  var gruntTaksUtils = opts.gruntTaskUtils;
+
+  return {
+    'changelog': {
+      dest: './report/changelog/changelog.html'
+      options: {
+        'gitUrlForCommit': 'http://git.kno.com/?p=cloud/kno-web-lib.git;a=commit;h={0}',
+        'gitAuthorUrl': 'http://git.kno.com/?p=cloud/kno-web-lib.git;a=search;s={0};st=author',
+        'urlForBugId': 'https://rally1.rallydev.com/#/search?keywords={0}'
+      }
+    }
+  };
+};
---
+++ grunt-deps/configs/install-hooks.js
@@ -0,0 +1,10 @@
+module.exports = function (grunt, pkg, opts) {
+  'use strict';
+  var gruntTaksUtils = opts.gruntTaskUtils;
+
+  return {
+    hook : {
+      gitHooksDirectory: '.git/hooks/'
+    }
+  };
+};
